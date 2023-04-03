c346508219688e3b1da79ac11e287eefe4d0f6d2
mfratello <michele.fratello@tuni.fi>
2023-04-03 16:52:42 +0300

    add scripts to run the app

---
+++ app.R
@@ -0,0 +1,3 @@
+source("ESPERANTO_app/server.R")
+source("ESPERANTO_app/ui.R")
+shinyApp(ui=ui, server=server)
---
+++ run_esperanto.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm -it -p 3838:3838 -v "$(pwd):/srv/shiny-server" esperanto
